2f748db90ff6ce9d60987eaaebe6aa53d802b621
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 09:19:13 +0900

    CharToDecimal.javaとそのclassファイルを追加(テキストの「値の設定を50に変更した場合」を適用)

---
+++ CharToDecimal/CharToDecimal.java
@@ -0,0 +1,11 @@
+public class CharToDecimal {
+    public static void main(String[] args) {
+        int num;
+        char ch;
+        
+        ch = 50;
+        num = ch;
+        
+        System.out.println("文字の \'" + ch + "\' は、10進数で " + num);
+    }
+}
