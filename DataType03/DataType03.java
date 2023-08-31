f9f9df96bba0db64422c6f681d7414c76765ea10
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 09:13:07 +0900

    DataType03.javaとそのclassファイルを追加(テキストの3個目の「変更してみます」を適用)

---
+++ DataType03/DataType03.java
@@ -0,0 +1,13 @@
+public class DataType03 {
+    public static void main(String[] args) {
+        int intVal;
+        double doubleVal;
+        
+        System.out.println("intVal = 10, doubleVal = 1.25で実行");
+        
+        intVal = 10;
+        doubleVal = 1.25;
+        doubleVal =intVal * doubleVal;
+        System.out.println("計算結果は " + doubleVal);
+    }
+}
